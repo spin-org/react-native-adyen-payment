84b81f5a23f8f5b9dc5eefa8abc3ab1f158f61be
Michael Link <mlink@falkor.com>
2021-08-20 15:46:44 -0500

    PAYX-2863: Add tipsi replacement methods to Adyen to support existing RN code

--- ios/AdyenPayment.m
+++ ios/AdyenPayment.m
@@ -1,8 +1,14 @@
 #import <React/RCTBridgeModule.h>
 #import <React/RCTEventEmitter.h>
+#import <React/RCTLog.h>
+@import PassKit;
 
 @interface RCT_EXTERN_MODULE(AdyenPayment, NSObject)
 RCT_EXTERN_METHOD(startPayment:(NSString *)component componentData:(NSDictionary *)componentData paymentDetails:(NSDictionary *)paymentDetails)
 RCT_EXTERN_METHOD(initialize:(NSDictionary *)appServiceConfigData)
 RCT_EXTERN_METHOD(startPaymentPromise:(NSString *)component componentData:(NSDictionary *)componentData paymentDetails:(NSDictionary *)paymentDetails resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(canMakeApplePayPayments)
+{
+    return @([PKPaymentAuthorizationViewController canMakePaymentsUsingNetworks:@[PKPaymentNetworkVisa, PKPaymentNetworkAmex, PKPaymentNetworkMasterCard, PKPaymentNetworkDiscover]]);
+}
 @end
