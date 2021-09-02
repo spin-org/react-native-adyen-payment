2afc9c8ac8b243ce0ed735fc24c0d4ecd14f6429
Michael Link <mlink@falkor.com>
2021-09-02 16:51:07 -0500

    [PAYX-2656] update Adyen RN interface to support additional line items for Apple Pay

--- ios/AdyenPayment.m
+++ ios/AdyenPayment.m
@@ -7,8 +7,8 @@
 RCT_EXTERN_METHOD(startPayment:(NSString *)component componentData:(NSDictionary *)componentData paymentDetails:(NSDictionary *)paymentDetails)
 RCT_EXTERN_METHOD(initialize:(NSDictionary *)appServiceConfigData)
 RCT_EXTERN_METHOD(startPaymentPromise:(NSString *)component componentData:(NSDictionary *)componentData paymentDetails:(NSDictionary *)paymentDetails resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
-RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(canMakeApplePayPayments)
+RCT_EXPORT_METHOD(canMakeApplePayPayments:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 {
-    return @([PKPaymentAuthorizationViewController canMakePaymentsUsingNetworks:@[PKPaymentNetworkVisa, PKPaymentNetworkAmex, PKPaymentNetworkMasterCard, PKPaymentNetworkDiscover]]);
+    resolve(@([PKPaymentAuthorizationViewController canMakePaymentsUsingNetworks:@[PKPaymentNetworkVisa, PKPaymentNetworkAmex, PKPaymentNetworkMasterCard, PKPaymentNetworkDiscover]]));
 }
 @end
